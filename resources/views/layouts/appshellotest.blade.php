a6f1df7c42376b7e72796594309d14844801fc46
Samson Teclezion <steclezion@gmail.com>
2026-01-28 02:08:58 -0800

    01-28-26-04

---
+++ resources/views/layouts/appshellotest.blade.php
@@ -0,0 +1,6 @@
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+
+</head>
