3911cfcc36eaa760a4bba62bbadd44487382791c
Samson Teclezion <steclezion@gmail.com>
2026-02-22 12:48:35 -0800

    02-22-26-03

--- resources/views/pages/auth/signup/signup.blade.php
+++ resources/views/pages/auth/signup/signup.blade.php
@@ -4,7 +4,10 @@
   <meta charset="utf-8">
   <title>Raymoch • Sign Up</title>
   {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
-<meta name="viewport" content="{{ csrf_token() }},width=device-width, initial-scale=1, viewport-fit=cover">
+  
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+
   @viteReactRefresh
   @vite('resources/js/app.jsx')
   <script>
